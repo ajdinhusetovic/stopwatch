d0bf633ba7b213ae21db3a6f1a5db2e1c3ded43e
Ajdin <ajdinhusetovic@outlook.com>
2023-02-18 15:06:55 +0100

    created basic functionality, stopwatch should work

--- app.js
+++ app.js
@@ -9,23 +9,53 @@ const startButton = document.querySelector('#start');
 const stopButton = document.querySelector('#stop');
 const resetButton = document.querySelector('#reset');
 
-let a = 0;
 
-let seconds = 00;
+
+let seconds = 50;
+let minutes = 59;
 let hours = 00;
 let interval;
 
 // functions
 
-const as = () => {
-    a++;
-    second.textContent = a;
+const start = () => {
+    seconds++;
+    if (seconds < 10) {
+        second.textContent = `0${seconds}`;
+    }
+    if (seconds > 9) {
+        second.textContent = seconds;
+    }
+    if (seconds > 59) {
+        minutes++;
+        seconds = 00;
+        minute.textContent = `0${minutes}`;
+        second.textContent = `0${seconds}`;
+    }
+    if (minutes < 10) {
+        minute.textContent = `0${minutes}`; 
+    }
+    if (minutes > 9) {
+        minute.textContent = minutes;
+    }
+    if (minutes > 59) {
+        hours++;
+        minutes = 00;
+        hour.textContent = `0${hours}`;
+        minute.textContent = `0${minutes}`;
+    }
+    if (hours < 10) {
+        hour.textContent = `0${hours}`;
+    }
+    if (hours > 9) {
+        hour.textContent = hours;
+    }
 }
 
 
 // event listeners
 startButton.addEventListener('click', () => {
-    interval = setInterval(as, 1000);
+    interval = setInterval(start, 1000);
     startButton.disabled = true;
 });
 
